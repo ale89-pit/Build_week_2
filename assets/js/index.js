77204f8dcb4bfa4dde915263898171b544409924
DekanisHani <dekanis@outlook.it>
2023-03-21 16:01:47 +0100

    dekanis

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,14 +59,14 @@ getMusic2();
 const writeSecondRow = function (musicList2) {
   for (let i = 0; i < 6; i++) {
     miniCard.innerHTML += `<div class="col col-6 col-lg-4">
-        <div class="card mb-3 text-bg-dark">
+        <div class="card mb-3 text-light smallCards">
             <div class="row g-0">
                 <div class="col-4">
                     <img src="${musicList2[i].album.cover}" class="img-fluid rounded-start"
                         alt="..." />
                 </div>
-                <div class="col-8">
-                    <h5 class="card-title">${musicList2[i].title}</h5>
+                <div class="col-8 d-flex justify-content-center align-items-center">
+                    <h5 class="card-title sizeTesto m-0 p-0">${musicList2[i].title}</h5>
                 </div>
             </div>
 
