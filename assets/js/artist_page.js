1a38e9f899a87b82b4ef2411b3e1b1649c5102ce
andrea-buonocore <123959335+andrea-buonocore@users.noreply.github.com>
2023-03-22 14:31:39 +0100

    fetch quasi completata

--- assets/js/artist_page.js
+++ assets/js/artist_page.js
@@ -1,18 +1,39 @@
 console.log('Funziona tutto!');
 
-const URLRequest = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
-let nameArtist = new URLSearchParams(window.location.search).get('artist');
-let artistId = new URLSearchParams(window.location.search).get('artistId')
+const URLRequest = " https://striveschool-api.herokuapp.com/api/deezer/artist/";
+
+let artistId = new URLSearchParams(window.location.search).get('artistId');
+
 console.log('artistId', artistId);
-console.log('artis name: ', nameArtist);
+
+
+let showArtistTracklist = async (artistTracklist) => {
+    try{
+        let response = await fetch(artistTracklist);
+        if(response.ok){
+            let tracklist = await response.json();
+            console.log(tracklist)
+        }
+        else{
+            return new Error ('Non riesco a recuperare la tracklist, errore!', response.status);
+        }
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
 let findArtist = async () => {
     try {
         let response = await fetch(URLRequest + artistId);
         if (response.ok) {
             console.log('Ho ricevuto i dati: ', response);
             let artist = await response.json();
-            console.log(artist);
-
+            console.log('Artist:', artist.name);
+            let artistTracklist = artist.tracklist;
+            console.log(artistTracklist);
+            showArtistTracklist(artistTracklist);
         }
         else {
             return new Error('Errore!', response.status);
